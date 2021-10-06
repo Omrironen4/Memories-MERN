c4f48f714545868cebfa1bd3682b209bbb9d6033
Omri <omri.ronen4@gmail.com>
2021-10-06 11:54:53 +0300

    added express, body parser, mongoose, and cors to npm

---
+++ server/index.js
@@ -0,0 +1,8 @@
+//   express, framework for creating the routing of the application.
+import express from 'express';
+//body-parser which enables us to send post requests.
+import bodyParser from 'body-parser';
+//mongoose for creating models for our posts.
+import mongoose from 'mongoose';
+//cors enables cross origin requests.
+import cors from 'cors';
